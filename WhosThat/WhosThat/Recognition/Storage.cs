1de82236b03fdbfd759fe72e0f77c893468364bf
AndrejSc-PC\AndrejSc <sciupakov@gmail.com>
2018-10-02 02:25:52 +0300

    Added EmguCV nuget, started implementing recognition

---
+++ WhosThat/WhosThat/Recognition/Storage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhosThat.Recognition
+{
+    public static class Storage
+    {
+        public static List<Face> Faces { get; set; } = new List<Face>();
+    }
+}
