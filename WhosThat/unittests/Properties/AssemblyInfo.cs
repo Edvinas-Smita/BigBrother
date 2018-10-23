0f9ccfbe1c5e6c3e8a9916bcf3fe945092cd93be
Teodoras Saulys <teodoras.saulys@gmail.com>
2018-10-23 10:10:58 -0700

    id factory unit test

---
+++ WhosThat/unittests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("unittests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("unittests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("741a3fb4-cdb6-4816-a8dd-82cf7de5af27")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WhosThat/unittests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace unittests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestIdFactory()
+        {
+            int initial_value = 4;
+            int expected = 5;
+
+            WhosThat.IdFactory.NextId = initial_value;
+
+            int actual = WhosThat.IdFactory.NextId;
+            Assert.AreEqual(actual, expected);
+
+        }
+    }
+}
