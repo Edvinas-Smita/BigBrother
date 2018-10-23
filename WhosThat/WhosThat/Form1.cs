edf10649a3ea740b913239aa0a205d041a37e023
Edvinas-Smita <36479307+Edvinas-Smita@users.noreply.github.com>
2018-10-24 01:59:07 +0300

    Changes focused around getting the user management tab to look like threads_old branch
    Copy pasted parts of required structure and changed stuff where needed to have most info in Storage.People (and bind user management tab fields to it)

--- WhosThat/WhosThat/Form1.cs
+++ WhosThat/WhosThat/Form1.cs
@@ -14,6 +14,9 @@ using Emgu.CV;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Face;
 using Emgu.CV.Structure;
+using WhosThat.Recognition;
+using WhosThat.Recognition.Util;
+using WhosThat.UserManagement.Util;
 
 namespace WhosThat
 {
@@ -27,7 +30,7 @@ namespace WhosThat
         private List<Image<Gray, byte>> Faces { get; set; }
         private List<int> IDs { get; set; }
         private int ProcessedImageWidth { get; set; } = 128;
-        private int ProcessedImageHeight { get; set; } = 150;
+        private int ProcessedImageHeight { get; set; } = 128;
         private int TimerCounter { get; set; } = 0;
         private int TimeLimit { get; set; } = 40;
         private int ScanCounter { get; set; } = 0;
@@ -49,11 +52,51 @@ namespace WhosThat
 
         List<Person> currentPeople = new List<Person>();
 
-        public Form1()
+
+	    private static string FaceHaarCascadePath = @"Recognition\HaarClassifiers\haarcascade_frontalface_default.xml";
+	    private static string EyeHaarCascadePath = @"Recognition\HaarClassifiers\haarcascade_eye.xml";
+	    private static string DetectorDataPath = @"TEST.xml";
+
+	    private Person loggedInUser = null;
+
+		private RecognizerEngine engine;
+
+		public Form1()
         {
             InitializeComponent();
 
-            FaceRecognition = new EigenFaceRecognizer(80, double.PositiveInfinity);
+	        EmguSingleton.Instance.SetUp
+	        (
+		        FaceHaarCascadePath,
+		        EyeHaarCascadePath,
+		        DetectorDataPath
+	        );
+
+			#region CustomDataBindings
+
+	        /*userNameTextBox.DataBindings.Add(new Binding("Text", loggedInUser, "Name"));	//since there is no 'loggedinuser' initially these values cant be bound - uncomment after we have a login which sets it
+	        userBioTextBox.DataBindings.Add(new Binding("Text", loggedInUser, "Bio"));
+	        userLikesTextBox.DataBindings.Add(new Binding("Text", loggedInUser, "Likes"));*/
+
+			userDebugNameCombo.SelectedIndex = -1;
+	        userDebugNameCombo.DataSource = new BindingSource(Storage.People, "");
+	        userDebugNameCombo.DisplayMember = "Name";
+	        userDebugNameCombo.SelectedIndexChanged += (IChannelSender, args) =>
+	        {
+		        loggedInUser = (Person) userDebugNameCombo.SelectedItem;
+				userNameTextBox.DataBindings.Clear();
+		        userBioTextBox.DataBindings.Clear();
+		        userLikesTextBox.DataBindings.Clear();
+		        userNameTextBox.DataBindings.Add(new Binding("Text", loggedInUser, "Name"));
+		        userBioTextBox.DataBindings.Add(new Binding("Text", loggedInUser, "Bio"));
+		        userLikesTextBox.DataBindings.Add(new Binding("Text", loggedInUser, "Likes"));
+				UtilStatic.SetupUserPicturePanel(loggedInUser, userPicturePanel);
+	        };
+
+	        #endregion
+
+			engine = new RecognizerEngine("", YMLPath);
+	        FaceRecognition = engine._faceRecognizer;//new EigenFaceRecognizer(80, double.PositiveInfinity);
 
             if (File.Exists(YMLPath) && new FileInfo(YMLPath).Length > 0)
             {
@@ -103,8 +146,11 @@ namespace WhosThat
                         {
                             var result = FaceRecognition.Predict(processedImage);
                             var label = CheckRecognizeResults(result, _threshold);
-                            imageFrame.Draw(label, face.Location, FontFace.HersheyTriplex, 1.0, new Bgr(Color.Chartreuse));
-                            Console.WriteLine();
+	                        var recognisedPerson = Storage.FindPersonByID(result.Label);
+	                        var personNameIfFound = recognisedPerson == null
+		                        ? "Spooky ghost no. " + result.Label.ToString()
+		                        : recognisedPerson.Name;
+							imageFrame.Draw(personNameIfFound, face.Location, FontFace.HersheyTriplex, 1.0, new Bgr(Color.Chartreuse));	//since we dont store person objects over restarts we wont have any info about the id
                         }
                         catch (Exception ex)
                         {
@@ -125,13 +171,14 @@ namespace WhosThat
         }
 
         private void btnAddNewFace_Click(object sender, EventArgs e)
-        {
-            if (txtNewFaceName.Text != string.Empty)
-            {
-                idToRemember = IdFactory.GetCurrentId();
-
-                Console.Write($"Training has started. {Environment.NewLine}");
-                Console.WriteLine("Person id is: "+ txtNewFaceName.Text);
+		{
+			Regex regex = new Regex(@"(.|\s)*\S(.|\s)*");
+			Match matchName = regex.Match(txtNewFaceName.Text);
+			if (matchName.Success)
+			{
+				Person person = new Person(txtNewFaceName.Text, "", "");
+				Console.Write($"Training has started. {Environment.NewLine}");
+                Console.WriteLine("Person id is: "+ idToRemember);
                 txtNewFaceName.Enabled = !txtNewFaceName.Enabled;
 
                 Timer = new Timer();
@@ -139,20 +186,22 @@ namespace WhosThat
                 Timer.Tick += Timer_Tick;
                 Timer.Start();
                 btnAddNewFace.Enabled = !btnAddNewFace.Enabled;
-            }
 
-            Person person = new Person(txtNewFaceName.Text, "", "");
-            listOfPeople.Add(person);
-            cmbNames.Items.Add(person.Name); //pridedu i kameros comboboxa
+				listOfPeople.Add(person);
+				cmbNames.Items.Add(person.Name); //pridedu i kameros comboboxa
 
-            cmbNamesInProfile.Items.Add(person.Name); //pridedu i profilio comboboxa
-            
-        }
+				//userDebugNameCombo.Items.Add(person.Name); //pridedu i profilio comboboxa
+				Storage.People.Add(person);
+			} else
+			{
+				MessageBox.Show("Invalid input");
+			}
+		}
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            Webcam.Retrieve(Frame);
-            var imageFrame = Frame.ToImage<Gray, byte>();
+            //Webcam.Retrieve(Frame);
+	        //var imageFrame = Frame.ToImage<Gray, byte>();
 
             //System.Diagnostics.Debug.WriteLine(TimerCounter);
 
@@ -160,7 +209,8 @@ namespace WhosThat
             {
                 TimerCounter++;
 
-                if (imageFrame != null)
+				Storage.People.Last().Images.Add(Frame.ToImage<Bgr, byte>().ToBitmap());	//bit wonky but whatever
+                /*if (imageFrame != null)
                 {
                     var faces = FaceDetection.DetectMultiScale(imageFrame, 1.3, 5);
                     
@@ -176,24 +226,26 @@ namespace WhosThat
                         Console.WriteLine($"{ScanCounter} Successful Scans Taken...");
                        
                     }
-                }
+                }*/
             }
             else
             {
-                if (Faces.Count > 0)
-                {
-                    System.Diagnostics.Debug.WriteLine("ADDED FACE IMAGES FOR TRAINING: " + Faces.ToArray().Length + '\n');
-                    System.Diagnostics.Debug.WriteLine("IDs array length " + IDs.ToArray().Length);
-                    FaceRecognition.Train(Faces.ToArray(), IDs.ToArray());
-                }
-                EndTraining(Faces.Count > 0);
+	            engine.TrainRecognizer();
+				EndTraining(true);
+	            /*if (Faces.Count > 0)
+	            {
+	                System.Diagnostics.Debug.WriteLine("ADDED FACE IMAGES FOR TRAINING: " + Faces.ToArray().Length + '\n');
+	                System.Diagnostics.Debug.WriteLine("IDs array length " + IDs.ToArray().Length);
+	                FaceRecognition.Train(Faces.ToArray(), IDs.ToArray());
+	            }
+	            EndTraining(Faces.Count > 0);*/
             }
         }
 
 
         private void EndTraining(bool facesDetected)
         {
-            FaceRecognition.Write(YMLPath);
+            //FaceRecognition.Write(YMLPath);
             Timer.Stop();
             TimerCounter = 0;
             btnAddNewFace.Enabled = !btnAddNewFace.Enabled;
@@ -214,25 +266,6 @@ namespace WhosThat
             var result = FaceRecognition.Predict(userImage.Resize(100, 100, Inter.Cubic));
             return result.Label;
         }
-
-        private void btnUpdateInfo_Click(object sender, EventArgs e)
-        {
-            Regex regex = new Regex(@"(.|\s)*\S(.|\s)*");
-            Match matchBio = regex.Match(txtBio.Text);
-            Match matchLikes = regex.Match(txtLikes.Text);
-            if (matchBio.Success && matchLikes.Success && cmbNamesInProfile.SelectedIndex!=-1)
-            {
-                int index = listOfPeople.FindIndex(x => x.Name.Equals(cmbNamesInProfile.Text));//kolkas padariau kad programa ieskotu reikiamo objekto is comboboxe pasirinkto vardo, ne perfect
-
-                listOfPeople[index].Bio = txtBio.Text;
-                listOfPeople[index].Likes = txtLikes.Text;
-
-                txtBio.Text = "";
-                txtLikes.Text = "";
-            }
-            else MessageBox.Show("Invalid input");
-        }
-
 
         private void recognizeButton_Click(object sender, EventArgs e)
         { 
@@ -305,6 +338,31 @@ namespace WhosThat
                 //isCameraTab = true;
                 Webcam.Start();
             }
-        }
-    }
+		}
+
+	    private void userAddPic_Click(object sender, EventArgs e)
+	    {
+		    var openFileDialog = new OpenFileDialog();
+		    openFileDialog.Multiselect = false; //TODO: add ability to choose multiple files
+		    openFileDialog.Title = "Pasirinkite nuotrauką";
+		    openFileDialog.Filter = "Image files|*.jpg;*.png";  //TODO: more image file extensions
+		    if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+		    {
+			    try
+			    {
+				    loggedInUser.Images.Add(new Bitmap(openFileDialog.FileName));
+			    }
+			    catch (Exception exception)
+			    {
+				    Console.WriteLine(exception);
+			    }
+		    }
+	    }
+
+		private void userDebugAddBlank_Click(object sender, EventArgs e)
+		{
+			Storage.People.Add(new Person("B L A N K", "", ""));
+			userDebugNameCombo.SelectedIndex = Storage.People.Count - 1;	//for some reason doesnt work if the blank value is the first one in the list [index=0]
+		}
+	}
 }
---
+++ WhosThat/WhosThat/UserManagement/Util/UtilStatic.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using WhosThat.Recognition;
+using WhosThat.Recognition.Util;
+
+namespace WhosThat.UserManagement.Util
+{
+	static class UtilStatic
+	{
+		public static void SetupUserPicturePanel(Person user, FlowLayoutPanel panel)
+		{
+			RepopulatePanel(user, panel);
+			//Statics.TrainSinglePersonFaces(user.Images, user.Id);   //TODO: this is just while we dont store any info anywhere
+			user.Images.CollectionChanged += (sender, args) => {
+				/*switch (args.Action)
+				{
+					case NotifyCollectionChangedAction.Add:
+						Statics.TrainSinglePersonFaces(args.NewItems, user.Id);
+						//train recognizer with the new images
+						break;
+					case NotifyCollectionChangedAction.Remove:
+						//untrain(?) recognier with the removed images
+						break;
+					case NotifyCollectionChangedAction.Replace:
+						//untrain old images and train new ones
+						break;
+					case NotifyCollectionChangedAction.Move:
+						//do nothing with the recognizer
+						break;
+					case NotifyCollectionChangedAction.Reset:
+						//delete (?) recognizer training for this user
+						break;
+				}*/
+				RepopulatePanel(user, panel);
+			};
+		}
+
+		private static void RepopulatePanel(Person user, FlowLayoutPanel panel) //Fills the user picture panel with the images from the user object
+		{
+			panel.Controls.Clear();
+			int realPanelWidth = panel.Width -
+								 panel.Margin.Horizontal -
+								 SystemInformation.VerticalScrollBarWidth;   //width for the panel to not show the horizontal scroll bar
+			for (int i = 0; i < user.Images.Count; ++i)
+			{
+				if (i != 0)
+				{
+					panel.Controls.Add(new Label()  //acts as a divisor between pictures
+					{
+						Text = "",
+						BorderStyle = BorderStyle.Fixed3D,
+						AutoSize = false,
+						Height = 2,
+						Width = realPanelWidth
+					});
+				}
+
+				var userImage = user.Images[i];
+				//	test - shows what face detector finds
+				/*var userImageFull = user.Images[i];
+				var userImageGray = new Image<Gray, byte>(userImageFull);
+				var faceRects = EmguSingleton.Instance.FaceDetector.DetectMultiScale(userImageGray, 1.3, 5);
+				var userImage = faceRects.Length > 0 ? userImageGray
+					.GetSubRect(EmguSingleton.Instance.FaceDetector.DetectMultiScale(userImageGray, 1.3, 5)[0])
+					.ToBitmap() : userImageFull;
+				userImageGray.Dispose();*/
+				//	test - shows what face detector finds
+
+				panel.Controls.Add(new PictureBox()
+				{
+					Image = userImage,
+					Size = new Size
+					(
+						realPanelWidth,
+						userImage.Height * realPanelWidth / userImage.Width
+					),  //picture box fitting the panel width and maintaining the picture aspect ratio
+					SizeMode = PictureBoxSizeMode.Zoom
+				});
+			}
+		}
+	}
+}
